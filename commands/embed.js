d6ab0588d9116d559d37dcc279e528280d2e3fc9
Xeoth <ujan.pl@hotmail.com>
2019-06-18 10:11:37 +0200

    Komenda embed działa

--- commands/embed.js
+++ commands/embed.js
@@ -1,26 +1,19 @@
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
-    
     const creationMessage = await message.channel.send("Wpisz tytuł embeda (masz na to 30 sekund!)")
 
     const filter = m => m.author.id === message.author.id
     message.channel.awaitMessages(filter, {max: 1, time: 30000}).then(collected => {
         const title = collected.first().content
-        //collected.react(":upvoteeye:")
-        //setTimeout(1000)
         collected.deleteAll(0)
         creationMessage.edit("Wpisz pole embeda (masz na to 30 sekund!)")
         message.channel.awaitMessages(filter, {max: 1, time:30000}).then(collected => {
             const fieldText = collected.first().content
-            //collected.react(":upvoteeye:")
-            //setTimeout(1000)
             collected.deleteAll(0)
             creationMessage.edit("Wpisz kolor embeda (masz na to 30 sekund!)")
             message.channel.awaitMessages(filter, {max: 1, time: 60000}).then(collected => {
                 const color = collected.first().content
-                //collected.react("<:upvoteeye:589888888936595497>")
-                //setTimeout(1000)
                 collected.deleteAll(0)
                 const embed = new Discord.RichEmbed()
                     .setTitle(title)
