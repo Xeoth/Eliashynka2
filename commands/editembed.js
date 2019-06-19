1b75b8b7e95da3e5d0df22d6766afb9905b4ce6e
Xeoth <ujan.pl@hotmail.com>
2019-06-19 23:50:40 +0200

    Sprawdzanie uprawnień w embedach

--- commands/editembed.js
+++ commands/editembed.js
@@ -1,34 +1,42 @@
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
-    const embedid = args[1]
-    const channelMention = message.mentions.channels.first()
-    if (!embedid) return message.reply("Nie wpisałeś ID embeda <:facepalm:589888884800880756>")
-    if (!channelMention) return message.reply("Nie wpisałeś nazwy kanału <:facepalm:589888884800880756>")
+    async function editEmbed() {
+        const embedid = args[1]
+        const channelMention = message.mentions.channels.first()
+        if (!embedid) return message.reply("Nie wpisałeś ID embeda <:facepalm:589888884800880756>")
+        if (!channelMention) return message.reply("Nie wpisałeś nazwy kanału <:facepalm:589888884800880756>")
 
-    const creationMessage = await message.channel.send("Wpisz tytuł embeda (masz na to 30 sekund!)")
-    channelMention.fetchMessage(embedid).then(messagea => {
-        const filter = m => m.author.id === message.author.id
-        message.channel.awaitMessages(filter, {max: 1, time: 30000}).then(collected => {
-            const title = collected.first().content
-            collected.deleteAll(0)
-            creationMessage.edit("Wpisz pole embeda (masz na to 30 sekund!)")
-            message.channel.awaitMessages(filter, {max: 1, time:30000}).then(collected => {
-                const fieldText = collected.first().content
+        const creationMessage = await message.channel.send("Wpisz tytuł embeda (masz na to 30 sekund!)")
+        channelMention.fetchMessage(embedid).then(messagea => {
+            const filter = m => m.author.id === message.author.id
+            message.channel.awaitMessages(filter, {max: 1, time: 30000}).then(collected => {
+                const title = collected.first().content
                 collected.deleteAll(0)
-                creationMessage.edit("Wpisz kolor embeda (masz na to 30 sekund!)")
-                message.channel.awaitMessages(filter, {max: 1, time: 60000}).then(collected => {
-                    const color = collected.first().content
+                creationMessage.edit("Wpisz pole embeda (masz na to 30 sekund!)")
+                message.channel.awaitMessages(filter, {max: 1, time:30000}).then(collected => {
+                    const fieldText = collected.first().content
                     collected.deleteAll(0)
-                    const embed = new Discord.RichEmbed()
-                        .setTitle(title)
-                        .setDescription(fieldText)
-                        .setColor(color)
-                    messagea.edit(embed)
-                    creationMessage.delete(0)
-                    message.delete(0)
+                    creationMessage.edit("Wpisz kolor embeda (masz na to 30 sekund!)")
+                    message.channel.awaitMessages(filter, {max: 1, time: 60000}).then(collected => {
+                        const color = collected.first().content
+                        collected.deleteAll(0)
+                        const embed = new Discord.RichEmbed()
+                            .setTitle(title)
+                            .setDescription(fieldText)
+                            .setColor(color)
+                        messagea.edit(embed)
+                        creationMessage.delete(0)
+                        message.delete(0)
+                    })
                 })
             })
         })
-    })
+    }
+    const perms = message.member.permissions
+    if (perms.has("ADMINISTRATOR")) {
+        editEmbed()
+    } else {
+        message.reply("Nie masz do tego uprawnień <:eee:589888887640555525>")
+    }
 };
--- commands/embed.js
+++ commands/embed.js
@@ -1,26 +1,34 @@
 const Discord = require("discord.js");
 
 exports.run = async (client, message, args) => {
-    const creationMessage = await message.channel.send("Wpisz tytuł embeda (masz na to 30 sekund!)")
+    async function createEmbed() {
+        const creationMessage = await message.channel.send("Wpisz tytuł embeda (masz na to 30 sekund!)")
 
-    const filter = m => m.author.id === message.author.id
-    message.channel.awaitMessages(filter, {max: 1, time: 30000}).then(collected => {
-        const title = collected.first().content
-        collected.deleteAll(0)
-        creationMessage.edit("Wpisz pole embeda (masz na to 30 sekund!)")
-        message.channel.awaitMessages(filter, {max: 1, time:30000}).then(collected => {
-            const fieldText = collected.first().content
+        const filter = m => m.author.id === message.author.id
+        message.channel.awaitMessages(filter, {max: 1, time: 30000}).then(collected => {
+            const title = collected.first().content
             collected.deleteAll(0)
-            creationMessage.edit("Wpisz kolor embeda (masz na to 30 sekund!)")
-            message.channel.awaitMessages(filter, {max: 1, time: 60000}).then(collected => {
-                const color = collected.first().content
+            creationMessage.edit("Wpisz pole embeda (masz na to 30 sekund!)")
+            message.channel.awaitMessages(filter, {max: 1, time:30000}).then(collected => {
+                const fieldText = collected.first().content
                 collected.deleteAll(0)
-                const embed = new Discord.RichEmbed()
-                    .setTitle(title)
-                    .setDescription(fieldText)
-                    .setColor(color)
-                creationMessage.edit(embed)
+                creationMessage.edit("Wpisz kolor embeda (masz na to 30 sekund!)")
+                message.channel.awaitMessages(filter, {max: 1, time: 60000}).then(collected => {
+                    const color = collected.first().content
+                    collected.deleteAll(0)
+                    const embed = new Discord.RichEmbed()
+                        .setTitle(title)
+                        .setDescription(fieldText)
+                        .setColor(color)
+                    creationMessage.edit(embed)
+                })
             })
         })
-    })
+    }
+    const perms = message.member.permissions
+    if (perms.has("ADMINISTRATOR")) {
+        createEmbed()
+    } else {
+        message.reply("Nie masz do tego uprawnień <:eee:589888887640555525>")
+    }
 };
