72581d90a528d64428f35724992acfce7bb05b0a
Xeoth <ujan.pl@hotmail.com>
2019-09-08 15:22:07 +0200

    Komenda działa

--- commands/changecolor.js
+++ commands/changecolor.js
@@ -1,13 +1,16 @@
 const Discord = require("discord.js");
-const colorFile = require('../rolecolors.json');
 const fs = require('fs');
 
 exports.run = (client, message, args) => {
-  //const chosenColor = args[0];
-  //if (chosenColor.length != 7 || chosenColor.startsWith("#")) return message.reply("Wpisz poprawny kod koloru, na przykład `#98f019`")
-  fs.readFile('rolecolors.json', 'utf8', (err, data) => {
+  const chosenColor = args[0];
+  if (chosenColor.length !== 7 || chosenColor.startsWith("#") === false) return message.reply("Wpisz poprawny kod koloru, na przykład `#98f019`")
+  fs.readFile('rolecolors.txt', 'utf8', (err, data) => {
     if (err) throw err;
-    console.log(data)
+    if (data.includes(message.author.id) === false) return message.reply("Nie masz przydzielonego koloru.")
   });
-  //if (colorFile)
+
+  const colorRole = message.guild.roles.find(r => r.name === message.author.id);
+  colorRole.edit({color: chosenColor}, `Zmieniono kolor na polecenie ${message.author.username}`)
+  
+  message.reply(`Zmieniono kolor na \`${chosenColor}\``)
 };
