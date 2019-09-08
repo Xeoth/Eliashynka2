87d7e503946eb4104647a939731951807b0521d9
Xeoth <ujan.pl@hotmail.com>
2019-09-08 15:27:16 +0200

    Nowy system sprawdzania ról

--- commands/changecolor.js
+++ commands/changecolor.js
@@ -4,12 +4,9 @@ const fs = require('fs');
 exports.run = (client, message, args) => {
   const chosenColor = args[0];
   if (chosenColor.length !== 7 || chosenColor.startsWith("#") === false) return message.reply("Wpisz poprawny kod koloru, na przykład `#98f019`")
-  fs.readFile('rolecolors.txt', 'utf8', (err, data) => {
-    if (err) throw err;
-    if (data.includes(message.author.id) === false) return message.reply("Nie masz przydzielonego koloru.")
-  });
-
+  
   const colorRole = message.guild.roles.find(r => r.name === message.author.id);
+  if (!colorRole) return message.reply("Nie masz przydzielonego koloru")
   colorRole.edit({color: chosenColor}, `Zmieniono kolor na polecenie ${message.author.username}`)
   
   message.reply(`Zmieniono kolor na \`${chosenColor}\``)
