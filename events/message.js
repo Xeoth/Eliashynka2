ee2ddc58da4cf6cf5d2355c95d05c76921c59e6e
Xeoth <ujan.pl@gmail.com>
2019-11-16 13:30:35 +0100

    Dodałem elementy bazy danych

--- events/message.js
+++ events/message.js
@@ -1,5 +1,20 @@
+const SQLite = require("better-sqlite3");
+const sql = new SQLite('./scores.sqlite');
+
 module.exports = (client, message) => {
   if (message.author.bot) return;
+  if (message.guild) {
+    let score = client.getScore.get(message.author.id, message.guild.id);
+    if (!score) {
+      score = {
+        id: `${message.guild.id}-${message.author.id}`,
+        user: message.author.id,
+        guild: message.guild.id,
+        points: 0,
+        level: 1
+      }
+    }
+  }
   if (message.content.indexOf(client.config.prefix) !== 0) return;
   const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
