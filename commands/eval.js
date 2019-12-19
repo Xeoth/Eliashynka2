5219f85a8a4e93212e0cef533d9a36072d6a9f08
Xeoth <ujan.pl@gmail.com>
2019-12-19 22:32:09 +0100

    Added eval

---
+++ commands/eval.js
@@ -0,0 +1,16 @@
+module.exports = (client, message) => {
+    const args = message.content.split(" ").slice(1);
+
+    if(message.author.id !== client.config.ownerID) return;
+    try {
+        const code = args.join(" ");
+        let evaled = eval(code);
+
+        if (typeof evaled !== "string")
+            evaled = require("util").inspect(evaled);
+
+        message.channel.send(clean(evaled), {code:"xl"});
+    } catch (err) {
+        message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+    }
+}
