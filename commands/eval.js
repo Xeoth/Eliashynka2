d18f6cb344315b1b252d178816f2e163cb56ab8d
Xeoth <ujan.pl@gmail.com>
2019-12-20 17:25:59 +0100

    Eval works

--- commands/eval.js
+++ commands/eval.js
@@ -6,17 +6,17 @@ const clean = text => {
 }
 
 exports.run = (client, message) => {
-    const args = message.content.split(" ").slice(1);
+    const args = message.content.split(" ").slice(2);
 
-    if(message.author.id !== client.config.ownerID) return message.reply("Wyjazd oszukańcu!");
+    if(message.author.id !== client.config.ownerID) return;
     try {
         const code = args.join(" ");
         let evaled = eval(code);
-
+        
         if (typeof evaled !== "string")
-            evaled = require("util").inspect(evaled);
-
-        message.channel.send(clean(evaled), {code:"xl"});
+        evaled = require("util").inspect(evaled);
+        
+        message.channel.send(clean(evaled), {code:"js"});
     } catch (err) {
         message.channel.send(`\`ERROR\` \`\`\`js\n${clean(err)}\n\`\`\``);
     }
