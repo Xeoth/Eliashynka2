3bb15bc56fdd7dbf47905ce8d81712fa52defbc1
Xeoth <ujan.pl@gmail.com>
2019-12-26 23:37:22 +0100

    Removed the upvote system

--- events/message.js
+++ events/message.js
@@ -3,30 +3,6 @@ const Discord = require("discord.js");
 
 module.exports = async (client, message) => {
     if (message.author.bot) return;
-    if (message.channel.id == client.config.memeChannel && message.attachments.size > 0) {
-        //Tracking emotes
-        const upvote = '👍';
-        const downvote = '👎';
-
-        await message.react(upvote);
-        await message.react(downvote);
-
-        const downvoteFilter = reaction => reaction.emoji.name === downvote;
-
-        const downvoteCollector = await message.createReactionCollector(downvoteFilter, { time: ms(client.config.timeToFinishCounting) });
-
-        downvoteCollector.on('collect', r => {
-            if (r.count == client.config.downvotesToDelete) {
-                return message.delete(0, `Mem uzyskał -${client.config.downvotesToDelete} punktów`);
-            }
-        })
-
-        downvoteCollector.on('end', () => {
-            if (message.deleted) return;
-            message.clearReactions();
-        })
-    }
-
     if (message.content.indexOf(client.config.prefix) !== 0) return;
     try {
         const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
