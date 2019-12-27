9c79cd9d51f920c1eb9c1f0b31d54810dc4365f5
Xeoth <ujan.pl@gmail.com>
2019-12-27 18:57:23 +0100

    Removed date as it's native to discord

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,6 +1,4 @@
 module.exports = (client, message) => {
-    const logChannel = client.channels.get(client.config.logChannel);
-    var date = new Date();
-    const currentTimeAndDate = `[${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} ${date.getDate()}.${date.getMonth()}.${date.getFullYear()}] - `;
-    logChannel.send(currentTimeAndDate + "Successfully activated the bot <:greenTick:643513035495047188>");
-}
+  const logChannel = client.channels.get(client.config.logChannel);
+  logChannel.send('Successfully activated the bot <:greenTick:643513035495047188>');
+};
