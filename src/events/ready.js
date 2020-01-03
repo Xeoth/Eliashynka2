cbf61f121c65d8a61c6cc25479fb9e8db284d18d
Xeoth <ujan.pl@gmail.com>
2020-01-03 23:47:00 +0100

    Added motd in ready

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,4 +1,16 @@
 module.exports = (client, message) => {
+  // Send the log notification
   const logChannel = client.channels.get(client.config.logChannel);
   logChannel.send('Successfully activated the bot <:greenTick:643513035495047188>');
+
+  // Set the status
+  // See https://discord.js.org/#/docs/main/stable/typedef/PresenceData
+  const settings = {
+    game: {
+      name: 'zawodzenia furfagów w piecach',
+      type: 'LISTENING',
+    },
+  };
+
+  client.user.setPresence(settings);
 };
