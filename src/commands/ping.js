76b92ec5c4cb36637a52bd80721f0214252da681
Xeoth <ujan.pl@gmail.com>
2020-01-03 23:48:56 +0100

    Ping works again

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,6 +1,5 @@
 exports.run = async (client, message, args) => {
   const msg = await message.channel.send('Ping?');
-  const latency = msg.msg.createdTimestamp - message.createdTimestamp;
   const apiLat = Math.round(client.ping);
-  msg.edit(`Pong! Opóźnienie to ${latency}ms. Opóźnienie API to ${apiLat}ms`);
+  msg.edit(`Pong! Opóźnienie API to ${apiLat}ms`);
 };
