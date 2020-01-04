b286721a6311b432056b7e1a27327b34c29edf46
Xeoth <ujan.pl@gmail.com>
2020-01-04 17:39:13 +0100

    Presence now changing every 6h

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,3 +1,7 @@
+// Require file with activities
+const names = require('../activities.json');
+const ms = require('ms');
+
 module.exports = (client, message) => {
   // Send the log notification
   const logChannel = client.channels.get(client.config.logChannel);
@@ -6,12 +10,19 @@ module.exports = (client, message) => {
   // Set the status
   // See https://discord.js.org/#/docs/main/stable/typedef/PresenceData
 
-  // Require file with activities
-  const names = require('../activities.json');
+  // Set a new activity every 6 hours
 
-  // Select a random activity
-  const settings = names[Math.floor(Math.random()*names.length)];
+  /** Used to set a new presence */
+  function setNewPresence() {
+    // Select a random activity
+    const settings = names[Math.floor(Math.random()*names.length)];
 
-  // Set the status
-  client.user.setPresence(settings);
+    // Set the status
+    client.user.setPresence(settings);
+  }
+
+  // Set the initiating presence
+  setNewPresence();
+
+  setInterval(setNewPresence, ms('6h'));
 };
