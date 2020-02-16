d9aa2fc66d0f7fab04a2c9a93b67f5326160fa51
Xeoth <ujan.pl@gmail.com>
2020-02-16 22:10:38 +0100

    Finished changecolor

--- src/commands/changecolor.js
+++ src/commands/changecolor.js
@@ -1,16 +1,39 @@
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
   const chosenColor = args[0];
+
   const regex = '^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$';
   if (!chosenColor.match(regex)) {
     return message.reply('Wpisz poprawny kod koloru, na przykład `#98f019`');
   }
 
-  const authorID = message.author.id;
-  const colorRole = message.guild.roles.find((r) => r.name === authorID);
-  if (!colorRole) return message.reply('Nie masz przydzielonego koloru');
-  colorRole.edit(
-      {color: chosenColor},
-      `Zmieniono kolor na polecenie ${message.author.username}`);
+  const author = message.guild.member(message.author);
+  const colorRole = message.guild.roles.find((r) => r.name === author.id);
 
-  message.reply(`Zmieniono kolor na \`${chosenColor}\``);
+  if (!colorRole) {
+    const statusMsg = await message.reply('Nie posiadasz aktualnie przydzielonego koloru. Próba autoprzydzielenia...');
+
+    // eslint-disable-next-line
+    const isSupporter = author.roles.some((r) => client.config.supporterRoles.includes(r.id));
+
+    if (isSupporter) {
+      const newRole = await message.guild.createRole({
+        name: author.id,
+        color: chosenColor,
+        permissions: 0,
+        position: client.config.supporterRolesPos,
+      });
+
+      author.addRole(newRole, 'Dodano rolę kolorów dla ' + message.author.username);
+
+      statusMsg.edit('<:greenTick:643513035495047188> Pomyślnie dodano rolę. Aktualny kolor:`' + chosenColor + '`');
+    } else {
+      statusMsg.edit('<:redCross:643513035402772520> Nie posiadasz roli uprawniającej do otrzymania koloru. Jeżeli uważasz, że to pomyłka, skontaktuj się z administracją.');
+    };
+  } else {
+    colorRole.edit({
+      color: chosenColor,
+    }, `Zmieniono kolor na polecenie ${message.author.username}`);
+
+    message.reply(`Zmieniono kolor na \`${chosenColor}\``);
+  };
 };
