135a4cc5d19e4e2f35dd1812c91e0f61c4ee77ad
Xeoth <ujan.pl@gmail.com>
2020-02-20 02:59:36 +0100

    Eslint got in my way

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -1,15 +1,18 @@
 module.exports = (client, member) => {
   if (!client.config.announcementsChannel) return;
+  
+  const memberCount = member.guild.memberCount;
 
-  if (!client.config.milestones.includes(member.guild.memberCount)) return;
+  if (!client.config.milestones.includes(memberCount)) return;
 
-  const announcementsChannel = member.guild.channels.find(c => c.id === client.config.announcementsChannel);
+  const announcementsChannel = member.guild.channels
+      .find((c) => c.id === client.config.announcementsChannel);
 
-  let milestoneMessage = client.config.milestoneMessage
+  const milestoneMessage = client.config.milestoneMessage
       .replace('$1', member.guild.name)
-      .split('$2') 
+      .split('$2')
       .join(member.guild.memberCount)
       .replace('$3', `<@${member.id}>`);
 
   announcementsChannel.send(milestoneMessage);
-}
+};
