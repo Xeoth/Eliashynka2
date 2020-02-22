b744882a5b12743ed6b06421ee598c5a6a49075e
Xeoth <ujan.pl@gmail.com>
2020-02-22 15:54:01 +0100

    Added catching

--- src/events/inviteCreate.js
+++ src/events/inviteCreate.js
@@ -1,7 +1,8 @@
 module.exports = (client, invite) => {
   if (invite.maxAge === 0) {
     if (invite.guild.member(invite.inviter).permissions.has('ADMINISTRATOR')) return;
-    invite.inviter.send('Nie zezwalamy na permanentne zaproszenia. Stwórz zaproszenie ponownie, ale wybierz skończoną ilość czasu wygaśnięcia.');
+    invite.inviter.send('Nie zezwalamy na permanentne zaproszenia. Stwórz zaproszenie ponownie, ale wybierz skończoną ilość czasu wygaśnięcia.')
+        .catch((err) => console.log('[INFO] Nie udało się wysłać wiadomości do ' + invite.inviter.username));
     invite.delete(`Zaproszenie było permanentne (${invite.inviter.username}).`);
   }
 };
